ba62f0792cb02f689a7982632152b2046bd611f8
NadellaRaviteja <ravitejan1506@gmail.com>
2024-09-12 21:44:57 +0530

    Initial Commit

---
+++ Movietrending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movietrending
+//
+//  Created by RAVITEJA NADELLA on 12/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
